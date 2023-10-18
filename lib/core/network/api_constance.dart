5d4c4c7c5a6d08590d2625b513075e78acb9a759
AbrarSaud <abrar.sauod0@gmail.com>
2023-10-18 23:14:34 +0300

    edit key

--- lib/core/network/api_constance.dart
+++ lib/core/network/api_constance.dart
@@ -1,6 +1,6 @@
 class ApiConstance {
   static const String baseUrl = "https://api.themoviedb.org/3";
-  static const String apiKey = "ffda6eaf7b1a371c3c387c82a0d3001e";
+  static const String apiKey = "---";
 
  static const String nowPlayingMoviesPath =
       "$baseUrl/movie/now_playing?api_key=$apiKey";
